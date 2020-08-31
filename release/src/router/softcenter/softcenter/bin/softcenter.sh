05e3964ba7aead7f088e19ff3bc93fa4b2943ed6
Xu Zhaowei <paldier@hotmail.com>
2020-08-31 15:16:57 +0800

    Bumped revision to 5.1.2

--- release/src/router/softcenter/softcenter/bin/softcenter.sh
+++ release/src/router/softcenter/softcenter/bin/softcenter.sh
@@ -4,8 +4,6 @@ source /jffs/softcenter/scripts/base.sh
 nvram set jffs2_scripts=1
 nvram commit
 
-#============================================
-# check start up scripts 
 dbus set softcenter_firmware_version=`nvram get extendno|cut -d "_" -f2|cut -d "-" -f1|cut -c2-6`
-#============================================
+
 
